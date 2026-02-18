ecf837c531506a2adb88339afcf60f3b7ff5703f
dinesh-sirmal <sirmaldiensh73@gmail.com>
2026-02-18 22:32:14 +0545

    Initial commit: Flask backend, Docker setup, and LaTeX report

---
+++ Backend/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host="db",
+    user="root",
+    password="root",
+    database="notesdb"
+)
+
+cursor = db.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS notes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT
+)
+""")
+
+@app.route("/")
+def home():
+    return "Notes Backend is running 🚀"
+
+@app.route("/add", methods=["POST"])
+def add_note():
+    data = request.json
+    content = data["content"]
+
+    cursor.execute("INSERT INTO notes (content) VALUES (%s)", (content,))
+    db.commit()
+
+    return jsonify({"message": "Note saved successfully!"})
+
+@app.route("/notes", methods=["GET"])
+def get_notes():
+    cursor.execute("SELECT * FROM notes")
+    result = cursor.fetchall()
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
